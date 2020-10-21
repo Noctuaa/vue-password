37e84b99571058172d9995cc088e25f264b30db8
Noctua <titgeogeo@gmail.com>
2020-10-21 18:02:48 +0200

    feat: generate random password

--- src/components/Password.vue
+++ src/components/Password.vue
@@ -1,14 +1,32 @@
 <template>
     <div class="inputBox">
         <h1>Password generator</h1>
-        <input type="password" name="" placeholder="Create Password" id="password">
-        <button class="btn">Generate</button>
+        <input type="password" name="" v-model="password" placeholder="Create Password" id="password">
+        <button class="btn" @click="generate">Generate</button>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return{
+                password: ''
+            }
+        },
 
+        methods: {
+            /**
+             * Generate Password
+             * code ASCII min = 33, max = 126
+             */
+            generate(){
+                this.password = '';
+                let passwordLength = 16;
+                for (let i = 0; i < passwordLength; i++) {                   
+                    this.password += String.fromCharCode(Math.floor(Math.random() * (126 - 33 + 1) + 33));
+                }
+            }
+        }
     }
 </script>
 
